c45e5bb5e8db2890719cd925b06b48b1ce0dc89e
Haskel <haskelchua@hotmail.com>
2020-11-09 20:01:32 +0800

    Added charts to th dashboard

--- pages/orders/dashboard.js
+++ pages/orders/dashboard.js
@@ -1,16 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { Panel } from "primereact/panel";
-import { Checkbox } from "primereact/checkbox";
-import { Button } from "primereact/button";
-import { Dropdown } from "primereact/dropdown";
-import { InputText } from "primereact/inputtext";
+import moment from "moment";
 import { Chart } from "primereact/chart";
-import { DataTable } from "primereact/datatable";
-import { Column } from "primereact/column";
 
 export default function THDashboard() {
   const [totalThaiNum, setTotalThaiNum] = useState(0);
   const [products, setProducts] = useState(null);
+  const [price, setPrice] = useState(0);
+  const [chartAllData, setChartAllData] = useState({});
 
   React.useEffect(() => {
     (async function getTotal() {
@@ -34,9 +30,97 @@ export default function THDashboard() {
         })
         .then((data) => {
           setProducts(data);
+          dailyPrice(data);
+          barChart(data);
         });
     })();
   }, []);
+
+  const dailyPrice = (data) => {
+    setPrice(0);
+    if (data) {
+      var currentPrice = price;
+      data.forEach((element) => {
+        var str = moment(element.order_datetime);
+
+        var today = moment();
+        if (today.isSame(str, "day")) {
+          currentPrice += element.price;
+        }
+      });
+      setPrice(currentPrice);
+    }
+  };
+
+  const barChart = (data) => {
+    var allDates = {};
+    var chartData = [];
+    var chartLabels = [];
+
+    if (data) {
+      //Get all dates in interval and set it to 0
+      var enddate = data[0].order_datetime;
+      var startdate = data[data.length - 1].order_datetime;
+      // If you want an inclusive end date (fully-closed interval)
+      for (var m = moment(startdate); m.isBefore(enddate); m.add(1, "days")) {
+        allDates[m.format("DD-MM-YYYY")] = {
+          name: m.format("DD-MM-YYYY"),
+          sales: 0,
+        };
+      }
+
+      // update the sale number of days which have sales
+      data.forEach((element) => {
+        var dateString = moment(element.order_datetime).format("DD-MM-YYYY");
+        allDates[dateString].sales++;
+      });
+
+      // push it into the charts
+      for (var key in allDates) {
+        var label = allDates[key].name;
+        var sales = allDates[key].sales;
+
+        chartLabels.push(label);
+        chartData.push(sales);
+      }
+
+      // set the labels for the chart
+      setChartAllData({
+        labels: chartLabels,
+        datasets: [
+          {
+            label: "Copies sold",
+            backgroundColor: "#66BB6A",
+            data: chartData,
+          },
+        ],
+      });
+    }
+  };
+
+  let basicOptions = {
+    legend: {
+      labels: {
+        fontColor: "#495057",
+      },
+    },
+    scales: {
+      xAxes: [
+        {
+          ticks: {
+            fontColor: "#495057",
+          },
+        },
+      ],
+      yAxes: [
+        {
+          ticks: {
+            fontColor: "#495057",
+          },
+        },
+      ],
+    },
+  };
 
   return (
     <div className="p-grid p-fluid dashboard">
@@ -49,9 +133,15 @@ export default function THDashboard() {
       </div>
       <div className="p-col-12 p-lg-6">
         <div className="card summary">
-          <span className="title">Revenue</span>
+          <span className="title">Daily Revenue</span>
           <span className="detail">Income for today</span>
-          <span className="count revenue">$0</span>
+          <span className="count revenue">{price}</span>
+        </div>
+      </div>
+      <div className="p-col-12 p-lg-12">
+        <div className="card">
+          <h5>th.buy2077.co - Number of copies sold</h5>
+          <Chart type="bar" data={chartAllData} options={basicOptions} />
         </div>
       </div>
     </div>
