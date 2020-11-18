241f53acf3697e245edf955362fc207393d407b0
Haskel Chua <haskelchua@hotmail.com>
2020-11-18 17:26:42 +0800

    Update _app.js
    reoriganized menu

--- pages/_app.js
+++ pages/_app.js
@@ -65,17 +65,12 @@ function MyApp({ Component, pageProps }) {
   };
 
   const menu = [
-    /*{
+    {
       label: "Dashboard",
       icon: "pi pi-fw pi-home",
       to: "/",
-    },*/
-     {
-       label: "Products",
-       icon: "pi pi-fw pi-circle-off",
-       to: "/products",
-     },
-    /*{
+    },
+    {
       label: "th.buy2077.co",
       icon: "pi pi-fw pi-list",
       items: [
@@ -90,7 +85,12 @@ function MyApp({ Component, pageProps }) {
           to: "/thbuy2077/orders",
         },
       ],
-    },*/
+    },
+                {
+                  label: "Products",
+                  icon: "pi pi-fw pi-circle-off",
+                  to: "/products",
+                },
     // {
     //   label: "sg.buy2077.co",
     //   icon: "pi pi-fw pi-list",
