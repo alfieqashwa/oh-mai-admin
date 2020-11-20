bfe177e7eb3d2e4bb21f2b67e9017e45665f1bc6
Haskel <haskelchua@hotmail.com>
2020-11-20 16:16:23 +0800

    Added new chart and sorting

--- components/helper.js
+++ components/helper.js
@@ -28,9 +28,11 @@ export function dailyPrice(setPrice, data) {
  * @param {*} setChartAllData function to set the labels and datasets of the bar chart
  * @param {*} data sql data for manipulation
  */
-export const initBarChart = (setChartAllData, data) => {
+export const initBarChart = (setChartAllData, data, filter, revenue) => {
   var allDates = {};
   var chartData = [];
+  var psData = [];
+  var xboxData = [];
   var chartLabels = [];
 
   if (data) {
@@ -48,32 +50,66 @@ export const initBarChart = (setChartAllData, data) => {
       allDates[m.format("DD-MM-YYYY")] = {
         name: m.format("DD-MM-YYYY"),
         sales: 0,
+        psSales: 0,
+        xboxSales: 0,
       };
     }
 
     // update the sale number of days which have sales
     data.forEach((element) => {
       var dateString = moment(element.order_datetime).format("DD-MM-YYYY");
-      if (element.order_status == "successful") allDates[dateString].sales++;
+      if (element.order_status == "successful") {
+        if (filter == "ALL" || element.kol == filter) {
+          if (element.platform == "PC") {
+            allDates[dateString].sales++;
+          } else if (element.platform == "PS4") {
+            allDates[dateString].psSales++;
+          } else if (element.platform == "XBOX") {
+            allDates[dateString].xboxSales++;
+          }
+        }
+      }
     });
 
     // push it into the charts
     for (var key in allDates) {
       var label = allDates[key].name;
       var sales = allDates[key].sales;
+      var psSales = allDates[key].psSales;
+      var xboxSales = allDates[key].xboxSales;
+
+      if (revenue) {
+        sales *= 1790;
+        psSales *= 1890;
+        xboxSales *= 1890;
+      }
 
       chartLabels.push(label);
+
       chartData.push(sales);
+      psData.push(psSales);
+      xboxData.push(xboxSales);
     }
 
+    console.log(allDates);
     // set the labels for the chart
     setChartAllData({
       labels: chartLabels,
       datasets: [
         {
-          label: "Copies sold",
-          backgroundColor: "#66BB6A",
+          label: "PC Sales",
+          backgroundColor: "#42A5F5",
           data: chartData,
+        },
+        {
+          label: "PS Sales",
+          backgroundColor: "#66BB6A",
+          data: psData,
+        },
+        {
+          label: "Xbox Sales",
+          backgroundColor: "#FFA726",
+          data: xboxData,
         },
       ],
     });
@@ -89,6 +125,7 @@ export let chartOptions = {
   scales: {
     xAxes: [
       {
+        stacked: true,
         ticks: {
           fontColor: "#495057",
         },
@@ -96,9 +133,46 @@ export let chartOptions = {
     ],
     yAxes: [
       {
+        stacked: true,
         ticks: {
           fontColor: "#495057",
           beginAtZero: true,
+        },
+      },
+    ],
+  },
+};
+
+export let chartPriceOptions = {
+  legend: {
+    labels: {
+      fontColor: "#495057",
+    },
+  },
+
+  scales: {
+    xAxes: [
+      {
+        stacked: true,
+        ticks: {
+          fontColor: "#495057",
+        },
+      },
+    ],
+    yAxes: [
+      {
+        stacked: true,
+        ticks: {
+          fontColor: "#495057",
+          beginAtZero: true,
+          callback: function (value, index, values) {
+            return new Intl.NumberFormat("th-TH", {
+              style: "currency",
+              currency: "THB",
+              minimumFractionDigits: 0,
+              maximumFractionDigits: 0,
+            }).format(value);
+          },
         },
       },
     ],
--- pages/thbuy2077/dashboard.js
+++ pages/thbuy2077/dashboard.js
@@ -1,12 +1,37 @@
 import React, { useState, useEffect } from "react";
 import moment from "moment";
 import { Chart } from "primereact/chart";
-import { dailyPrice, initBarChart, chartOptions } from "components/helper";
+import {
+  dailyPrice,
+  initBarChart,
+  chartOptions,
+  chartPriceOptions,
+} from "components/helper";
+import { Toolbar } from "primereact/toolbar";
+import { Dropdown } from "primereact/dropdown";
 
 export default function THDashboard() {
   const [totalThaiNum, setTotalThaiNum] = useState(0);
   const [price, setPrice] = useState(0);
   const [chartAllData, setChartAllData] = useState({});
+  const [chartRevData, setChartRevData] = useState({});
+  const [filterValue, setFilterValue] = useState("ALL");
+
+  const [currData, setCurrData] = useState([]);
+
+  const filterItems = [
+    { label: "All", value: "ALL" },
+    { label: "Chicken Show", value: "Chickenshow" },
+    { label: "Cyberclasher", value: "Cyberclasher" },
+    { label: "Gladiuz KB", value: "Gladiuz KB" },
+    { label: "Gufunnarock", value: "Gufunnarock" },
+    { label: "Hua Hed", value: "Hua Hed" },
+    { label: "Special", value: "Special" },
+  ];
+  function optionTemplate(option) {
+    if (option.value == "ALL") return <div>{option.label}</div>;
+    else return <div style={{ marginLeft: "20%" }}>{option.label}</div>;
+  }
 
   React.useEffect(() => {
     (async function getTotal() {
@@ -30,10 +55,38 @@ export default function THDashboard() {
         })
         .then((data) => {
           dailyPrice(setPrice, data);
-          initBarChart(setChartAllData, data);
+          setCurrData(data);
+          initBarChart(setChartAllData, data, "ALL", false);
+          initBarChart(setChartRevData, data, "ALL", true);
         });
     })();
   }, []);
+
+  const filterRightToolbarTemplate = () => {
+    return (
+      <React.Fragment>
+        <Dropdown
+          style={{ width: "150px" }}
+          value={filterValue}
+          options={filterItems}
+          onChange={(e) => {
+            setFilterValue(e.value);
+            initBarChart(setChartAllData, currData, e.value, false);
+            initBarChart(setChartRevData, currData, e.value, true);
+          }}
+          optionLabel="label"
+          itemTemplate={optionTemplate}
+        />
+      </React.Fragment>
+    );
+  };
+  const filterLeftToolbarTemplate = () => {
+    return (
+      <React.Fragment>
+        <h5>Bar Chart Filter Controls:</h5>
+      </React.Fragment>
+    );
+  };
 
   return (
     <div className="p-grid p-fluid dashboard">
@@ -48,13 +101,33 @@ export default function THDashboard() {
         <div className="card summary">
           <span className="title">th.buy2077.co Revenue</span>
           <span className="detail">Income for purchases</span>
-          <span className="count revenue">฿{price}</span>
+          <span className="count revenue">
+            {new Intl.NumberFormat("th-TH", {
+              style: "currency",
+              currency: "THB",
+              minimumFractionDigits: 0,
+              maximumFractionDigits: 0,
+            }).format(price)}
+          </span>
         </div>
       </div>
       <div className="p-col-12 p-lg-12">
+        <Toolbar
+          right={filterRightToolbarTemplate}
+          left={filterLeftToolbarTemplate}
+        ></Toolbar>
+      </div>
+
+      <div className="p-col-12 p-lg-6">
         <div className="card">
           <h5>th.buy2077.co - Number of copies sold</h5>
           <Chart type="bar" data={chartAllData} options={chartOptions} />
+        </div>
+      </div>
+      <div className="p-col-12 p-lg-6">
+        <div className="card">
+          <h5>th.buy2077.co - Revenue of copies sold</h5>
+          <Chart type="bar" data={chartRevData} options={chartPriceOptions} />
         </div>
       </div>
     </div>
