b57c8de807fdb007097f8b992c8855394c7e292b
alfieqashwa <alfieqashwa@gmail.com>
2021-04-13 17:10:48 +0700

    import layout

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,13 @@
-import React from "react";
-import Head from "next/head";
-import PropTypes from "prop-types";
-import CornersDiv from "components/cornersdiv";
-import { GlassDiv } from "components/glassdiv";
+import { GlassDiv } from 'components/glassdiv';
+import Layout from 'components/dashboardAdminLayout'
 
 function Index(props) {
   return (
-    <div>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div
-        className="bg-N800 w-screen h-screen flex flex-col justify-center items-center bg-center bg-cover"
-        style={{ backgroundImage: "url(/mainbg.png)" }}
-      >
-        <CornersDiv>
-          <GlassDiv>
-            <h1 className="pt-8">Admin Page Updating</h1>
-          </GlassDiv>
-        </CornersDiv>
-      </div>
-    </div>
+    <Layout title="Admin Home">
+      <h3 className="text-N0">
+        Home Admin Dashboard
+      </h3>
+    </Layout>
   );
 }
 
