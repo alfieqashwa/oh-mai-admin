671ad3c89e62addc303394c1e5687c990b829422
alfieqashwa <alfieqashwa@gmail.com>
2021-04-20 10:22:42 +0700

    custom basic rich-text-editor

--- components/textEditor.js
+++ components/textEditor.js
@@ -5,7 +5,14 @@ import 'suneditor/dist/css/suneditor.min.css'
 const TextEditor = props => {
   return (
     <div className="p-2">
-      <SunEditor />
+      <SunEditor
+        lang="en"
+        name="description"
+        defaultValue="<p>Enter description here</p>"
+        width="100%"
+        height="100%"
+        autoFocus={true}
+      />
     </div>
   )
 }
