0298a7c33d6513aced789c638742df753940594e
alfieqashwa <alfieqashwa@gmail.com>
2021-04-30 10:30:22 +0700

    increase the 3button

--- pages/analytics/summary/index.js
+++ pages/analytics/summary/index.js
@@ -218,7 +218,7 @@ export default function Summary() {
         <div className="flex items-center justify-between my-4" >
           <h4 className="w600">Performance</h4>
           <div className="w-full mx-5 border border-N0 border-opacity-30"></div>
-          <BsThreeDotsVertical className="w-5 h-5 mr-2 text-N0" />
+          <BsThreeDotsVertical className="w-6 h-6 mr-2 text-N0" />
         </div>
 
         <div>
