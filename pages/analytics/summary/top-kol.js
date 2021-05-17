9812d2a34a2b404f11150a095fdbb7302b577003
alfieqashwa <alfieqashwa@gmail.com>
2021-05-17 16:59:07 +0700

    created top-customer page

---
+++ pages/analytics/summary/top-kol.js
@@ -0,0 +1,31 @@
+import { GlassHeader } from 'components/glassHeader';
+import { Header } from 'components/header';
+import { TitleWithBackButton } from 'components/titleWithBackButton';
+import { LeaderBoardBorder, PaginationSummary } from 'components/analytics/summary';
+
+export default function TopKOL() {
+  return (
+    <div className="pr-12 pl-7">
+      {/* Header? */}
+      <Header title="Analytics - Summary - Top KOL" />
+      <GlassHeader title="top KOL">
+        <div className="flex space-x-4">
+          <button className="px-5 py-2 uppercase bg-transparent border w250-m text-N0">export</button>
+        </div>
+      </GlassHeader>
+
+      {/* Title */}
+      <TitleWithBackButton path="/analytics/summary" title="top KOL" />
+
+      {/* Leaderboard Border */}
+      <LeaderBoardBorder />
+
+      {/* Table */}
+      <h1 className="flex items-center justify-center h-48 my-16">TODO: TABLE LIST</h1>
+
+      {/* Pagination */}
+      <PaginationSummary />
+
+    </div>
+  )
+}
