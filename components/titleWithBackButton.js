00ad300f2265a8d5a2757e6f368b342802e67650
alfieqashwa <alfieqashwa@gmail.com>
2021-05-17 16:58:00 +0700

    [REFACTOR] -- created reusable component that can be use for entire pages

---
+++ components/titleWithBackButton.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import { AiOutlineArrowLeft } from 'react-icons/ai';
+
+export const TitleWithBackButton = ({ path, title }) => (
+  <div className="flex items-center mt-20" >
+    <Link href={path}>
+      <a>
+        <AiOutlineArrowLeft className="w-6 h-6 transition duration-300 ease-in-out text-N0 hover:text-P400" />
+      </a>
+    </Link>
+    <h3 className="ml-4 capitalize w700">{title}</h3>
+  </div>
+)
