fe4a3656d8862ba61ae6f2179a7e02f36c1b2397
azwar <azwar.akbar@gmail.com>
2021-06-23 20:46:42 +0700

    Fix ambigous home page and menu

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,23 @@
 import { Header } from 'components/header';
 import Cookies from 'js-cookie';
+import router from 'next/router';
 import { useEffect } from 'react';
+import { isTokenExist } from 'utils/Auth';
 
 function Index(props) {
-
   useEffect(() => {
-    // Cookies.set('token', "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwMGU2ZjBkMjlhZWNkMDY2OWQyMWFlMiIsImZpcnN0X25hbWUiOiJBendhciIsImxhc3RfbmFtZSI6IkFrYmFyIiwiZW1haWwiOiJhendhci5ha2JhckBnbWFpbC5jb20iLCJjbGllbnRfaWQiOiI2MGQxYzVkNWZiZDFmOTJmYjhjNDJlMDYiLCJpYXQiOjE2MjQzNjA0MDUsImV4cCI6MTYyNDk2NTIwNX0.11agT1_1pq63AQyx_3e-Mj9olu14RrAzSXP4G1d3iez")
+    if (isTokenExist()) {
+      router.push("/analytics/summary")
+    } else {
+      router.push("/login")
+    }
   }, [])
 
   return (
     <>
       <Header title="Home" />
       <div className="grid h-screen place-items-center">
-        <h1>Home Page</h1>
+        <h1>Welcome</h1>
       </div>
     </>
   );
