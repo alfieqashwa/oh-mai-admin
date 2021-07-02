262a68c3c2624a95fc8565027367c2ac69d78b39
alfieqashwa <alfieqashwa@gmail.com>
2021-07-02 15:06:27 +0700

    fetching getListTopSalesProduct

--- graphql/product.js
+++ graphql/product.js
@@ -125,8 +125,7 @@ export const DELETE_PRODUCT = `mutation deleteProduct(
   `
 
 export const GET_PRODUCT_GQL = gql`
-  query getProductByID($id: ID)
-  {
+  query getProductByID($id: ID) {
     getProductByID(id: $id) {
       id
       sku
@@ -147,66 +146,82 @@ export const GET_PRODUCT_GQL = gql`
       is_physical
       weight
     }
-  }`
+  }
+`
 
 export const UPDATE_PRODUCT_GQL = gql`
-mutation updateProduct(
-  $sku: String
-  $product_name: String
-  $base_price: Float
-  $sale_price: Float
-  $slug: String
-  $description: String
-  $on_sale: Boolean
-  $stock_quantity: Int
-  $stock_status: String
-  $categories: [String]
-  $tags: [String]
-  $new_featured_image: Upload
-  $images_file: [Upload]
-  $kol_profit: Int
-  $company_profit: Int
-  $is_physical: Boolean
-  $weight: Float
-  $active_status: Boolean
-)
-{
-  updateProduct(
-    sku: $sku
-    product_name: $product_name
-    base_price: $base_price
-    sale_price: $sale_price
-    slug: $slug
-    description: $description
-    on_sale: $on_sale
-    stock_quantity: $stock_quantity
-    stock_status: $stock_status
-    categories: $categories
-    tags: $tags
-    new_featured_image: $new_featured_image
-    images_file: $images_file
-    kol_profit: $kol_profit
-    company_profit: $company_profit
-    is_physical: $is_physical
-    weight: $weight
-    active_status: $active_status
+  mutation updateProduct(
+    $sku: String
+    $product_name: String
+    $base_price: Float
+    $sale_price: Float
+    $slug: String
+    $description: String
+    $on_sale: Boolean
+    $stock_quantity: Int
+    $stock_status: String
+    $categories: [String]
+    $tags: [String]
+    $new_featured_image: Upload
+    $images_file: [Upload]
+    $kol_profit: Int
+    $company_profit: Int
+    $is_physical: Boolean
+    $weight: Float
+    $active_status: Boolean
   ) {
-    sku
-    product_name
-    base_price
-    sale_price
-    cost_price
-    slug
-    description
-    on_sale
-    stock_quantity
-    stock_status
-    categories
-    tags
-    kol_profit
-    company_profit
-    is_physical
-    weight
-    active_status
+    updateProduct(
+      sku: $sku
+      product_name: $product_name
+      base_price: $base_price
+      sale_price: $sale_price
+      slug: $slug
+      description: $description
+      on_sale: $on_sale
+      stock_quantity: $stock_quantity
+      stock_status: $stock_status
+      categories: $categories
+      tags: $tags
+      new_featured_image: $new_featured_image
+      images_file: $images_file
+      kol_profit: $kol_profit
+      company_profit: $company_profit
+      is_physical: $is_physical
+      weight: $weight
+      active_status: $active_status
+    ) {
+      sku
+      product_name
+      base_price
+      sale_price
+      cost_price
+      slug
+      description
+      on_sale
+      stock_quantity
+      stock_status
+      categories
+      tags
+      kol_profit
+      company_profit
+      is_physical
+      weight
+      active_status
+    }
   }
-}`
+`
+
+// new schema
+export const GET_LIST_TOP_SALES_PRODUCT = gql`
+  query getListTopSalesProduct($startDate: String, $endDate: String) {
+    getListTopSalesProduct(startDate: $startDate, endDate: $endDate) {
+      sn
+      product_title
+      sku
+      item_sold
+      net_sales
+      order
+      category
+    }
+  }
+`
