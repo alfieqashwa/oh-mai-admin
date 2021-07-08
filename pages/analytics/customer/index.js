3a216d12dcdcb5820ddca654d6271ee1c09ed162
alfieqashwa <alfieqashwa@gmail.com>
2021-07-08 22:35:07 +0700

    fetch customer

--- pages/analytics/customer/index.js
+++ pages/analytics/customer/index.js
@@ -1,16 +1,54 @@
 import React, { useEffect, useState } from 'react'
 import { BsThreeDotsVertical } from 'react-icons/bs'
 import { Header } from 'components/header'
-import { DateRangeSelect, TableOrders, Pagination, ChartView, OrderPerformanceCard } from 'components/analytics/customer'
+import {
+  DateRangeSelect,
+  TableOrders,
+  Pagination,
+  ChartView,
+  OrderPerformanceCard,
+} from 'components/analytics/customer'
+
 import { checkLogin } from 'utils/Auth'
+import { getClient } from 'lib/graphqlclient'
+import { GET_ANALYTIC_CUSTOMER_TABLE } from 'graphql/customer'
 
 export default function Customer() {
   const [selectedCurrent, setSelectedCurrent] = useState(dates[0])
   const [selectedPrevious, setSelectedPrevious] = useState(dates[1])
 
+  const [getAnalyticCustomerTable, setGetAnalyticCustomerTable] = useState()
+
   useEffect(() => {
     checkLogin()
   }, [])
+
+  const client = getClient()
+
+  async function loadData() {
+    try {
+      const resultGetAnalyticCustomerTable = await client.request(
+        GET_ANALYTIC_CUSTOMER_TABLE
+      )
+      setGetAnalyticCustomerTable(
+        resultGetAnalyticCustomerTable.getAnalyticCustomerTable
+      )
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    loadData()
+  }, [])
+
+  useEffect(() => {
+    if (getAnalyticCustomerTable) {
+      console.log(
+        `getAnalyticCustomerTable_customerID, ${getAnalyticCustomerTable[0]?.customer_id}`
+      )
+    }
+  }, [getAnalyticCustomerTable])
 
   return (
     <div className="pb-4">
@@ -37,7 +75,9 @@ export default function Customer() {
         <Pagination />
         {/* customer_performance-border */}
         <div className="flex items-center justify-between mt-4 mb-7">
-          <h4 className="capitalize w600 whitespace-nowrap">customer performance</h4>
+          <h4 className="capitalize w600 whitespace-nowrap">
+            customer performance
+          </h4>
           <div className="w-full mx-5 border border-N0 border-opacity-30"></div>
           <BsThreeDotsVertical className="w-6 h-6 mr-2 text-N0" />
         </div>
@@ -45,7 +85,6 @@ export default function Customer() {
         <OrderPerformanceCard />
         {/* chart */}
         <ChartView />
-
       </div>
     </div>
   )
@@ -53,5 +92,5 @@ export default function Customer() {
 
 const dates = [
   { name: 'Current Year (Jan 1 - Dec 31, 2021)' },
-  { name: 'Previous Year (Jan 1 - Dec 31, 2020)' }
+  { name: 'Previous Year (Jan 1 - Dec 31, 2020)' },
 ]
