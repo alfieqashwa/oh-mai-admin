fcfa83941a9edbf3fc7af1a7c531dbb59222344b
azwar <azwar.akbar@gmail.com>
2021-07-27 19:06:00 +0700

    Update SwitchOnOff component for customer page. Add ability to update array on change value

--- components/analytics/customer/switch-on-off.js
+++ components/analytics/customer/switch-on-off.js
@@ -1,26 +1,42 @@
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
 import { Switch } from '@headlessui/react'
 import { CheckIcon } from '@heroicons/react/solid'
 import { XIcon } from '@heroicons/react/outline'
 
 export function SwitchOnOff(props) {
-  const initialValue = props.isEnabled
-  const [enabled, setEnabled] = useState(initialValue)
+  const customerId = props.customerId
+
+  useEffect(() => {
+    // console.log('re render...' + customerId)
+  }, [])
+
+  useEffect(() => {
+    // console.log('props.isEnabled', props.isEnabled)
+    // console.log('on togle change, cust id:' + customerId)
+  }, [props.isEnabled])
+
+  const onChange = () => {
+    // console.log('{___________on togle change')
+    // console.log('on togle change, cust id:' + customerId)
+    // console.log('on togle change, enabled:' + !props.isEnabled)
+    // console.log('on togle change___________}')
+    props.updateArray(customerId, !props.isEnabled)
+  }
 
   return (
     <Switch
-      checked={enabled}
-      onChange={setEnabled}
-      className={`${enabled ? 'bg-G400' : 'bg-N400'}
+      checked={props.isEnabled}
+      onChange={onChange}
+      className={`${props.isEnabled ? 'bg-G400' : 'bg-N400'}
         inline-flex flex-shrink-0 h-[36px] w-[82px] border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-N0 focus-visible:ring-opacity-75`}
     >
       <span className="sr-only">{props.title}</span>
       <span
         aria-hidden="true"
-        className={`${enabled ? 'translate-x-11' : 'translate-x-0'}
+        className={`${props.isEnabled ? 'translate-x-11' : 'translate-x-0'}
           pointer-events-none flex items-center justify-center h-[24px] w-[24px] rounded-full bg-N0 shadow-lg transform ring-0 transition ease-in-out duration-500`}
       >
-        {enabled
+        {props.isEnabled
           ? <CheckIcon className="w-5 h-5 text-G400" />
           : <XIcon className="w-5 h-5 text-N400" />
         }
