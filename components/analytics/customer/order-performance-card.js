95c83dc6e2629ff0efd8f5ffb1cdc00c613e0688
azwar <azwar.akbar@gmail.com>
2021-07-27 19:02:46 +0700

    Update file: order-performance-card. Refactor the component, fixing number format, implement graphql data result

--- components/analytics/customer/order-performance-card.js
+++ components/analytics/customer/order-performance-card.js
@@ -1,13 +1,59 @@
-import React, { useState } from 'react'
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react'
 import { RadioGroup } from '@headlessui/react'
 import { FiArrowDownRight, FiArrowRight, FiArrowUpRight } from 'react-icons/fi'
+import { GET_ORDER_CUSTOMER_YEARLY } from 'graphql/customer'
+// import useFetch from 'hooks/useFetch'
+import { loadPerformanceAnalyticCustomer } from 'services/api/analytics_customer'
+import { ArrowDirections } from 'components/widgets/PerformanceArrow'
+import { moneyFormat } from 'utils/money-format'
 
-export const OrderPerformanceCard = () => {
-  const [selected, setSelected] = useState(performanceCards[0])
+export const OrderPerformanceCard = (props) => {
+  const [selected, setSelected] = useState()
+  const [performances, setPerformances] = useState([])
+  const [dataArr, setDataArr] = useState([])
+  const arr = []
+
+  // const {
+  //   loading: loadgOrderCust,
+  //   error: errOrderCust,
+  //   data: performanceOrderCust
+  // } = useFetch(GET_ORDER_CUSTOMER_YEARLY)
+
+  // useEffect(() => {
+  //   console.log('OrderPerformanceCard/performanceOrderCust', 'init')
+
+  //   if (performanceOrderCust) {
+  //     console.log('OrderPerformanceCard/performanceOrderCust', performanceOrderCust)
+  //     arr.push(performanceCards)
+  //     const tmpArr = dataArr.concat(arr)
+  //     setDataArr(tmpArr)
+  //   }
+  // }, [])
+
+  useEffect(async () => {
+    console.log('OrderPerformanceCard/init/props.customerId', props)
+    // const customerId = props.selectedCustomer?.customer_id
+    // const result = await loadPerformanceAnalyticCustomer({ startDate: null, endDate: null, customerId: customerId })
+    // console.log('OrderPerformanceCard', result.data)
+    // setPerformances(result.data)
+  }, [])
+
+  useEffect(async () => {
+    console.log('>OrderPerformanceCard/[props.selectedCustomer]/props.customerId', props?.selectedCustomer?.customer_id)
+    if (props.selectedCustomer) {
+      const customerId = props.selectedCustomer.customer_id
+
+      const result = await loadPerformanceAnalyticCustomer({ startDate: null, endDate: null, customerId: customerId })
+      console.log('OrderPerformanceCard', result.data)
+      setPerformances(result.data)
+    }
+  }, [props.selectedCustomer])
+
   return (
     <RadioGroup className="grid grid-cols-3" value={selected} onChange={setSelected}>
       <RadioGroup.Label className="sr-only">Performance</RadioGroup.Label>
-      {performanceCards.map((c, i) => {
+      {performances.map((c, i) => {
         return (
           <RadioGroup.Option
             key={i}
@@ -26,36 +72,24 @@ export const OrderPerformanceCard = () => {
           >
             {({ active, checked }) => (
               <>
-                <h5 className={`mt-5 text-opacity-50 w250 ${checked ? 'text-N800' : 'text-N0'}`}>{c.category}</h5>
+                <h5 className={`mt-5 text-opacity-50 w250 ${checked ? 'text-N800' : 'text-N0'}`}>{c.title}</h5>
                 <div className="mt-3">
                   <h3 className={`w700 ${checked ? 'text-N800' : 'text-N0'}`}>
-                    {c.category === 'net sales'
-                      ? <>$NT{c.amount.toFixed(2)}</>
-                      : <>${c.amount.toFixed(2)}</>
+                    {c.type === 'currency'
+                      ? <>{moneyFormat.format(c.performance.toFixed(2))}</>
+                      : <>{c.performance.toFixed(2)}</>
                     }
                   </h3>
                   <div className="flex items-center space-x-1">
                     {/* temporary logic */}
-                    {c.percentage === '-'
-                      ? <>
-                        <FiArrowRight className={`w-5 h-5 ${checked ? 'text-N800' : 'text-N0'}`} />
-                        <h5 className={`w250 ${checked ? 'text-N800' : 'text-N0'}`}>{c.percentage}</h5>
-                      </>
-                      : c.category === 'orders'
-                        ? <>
-                          <FiArrowDownRight className="w-5 h-5 text-R600" />
-                          <h5 className="w250 text-R600">{c.percentage}%</h5>
-                        </>
-                        : <>
-                          <FiArrowUpRight className="w-5 h-5 text-G400" />
-                          <h5 className="w250 text-G400">{c.percentage}%</h5>
-                        </>
-                    }
+                    <ArrowDirections item={c} checked={checked} />
                   </div>
                 </div>
                 <div className="my-4">
                   <p className={`text-opacity-50 w400 ${checked ? 'text-N800' : 'text-N0'}`}>Previous Year</p>
-                  <p className={`w400 ${checked ? 'text-N800' : 'text-N0'}`}>${c.previousYear.toFixed(2)}</p>
+                  <p className={`w400 ${checked ? 'text-N800' : 'text-N0'}`}>
+                    {c.type === 'currency' ? moneyFormat.format(c.performance_last_year.toFixed(2)) : c.performance_last_year.toFixed(2)}
+                  </p>
                 </div>
               </>
             )}
@@ -65,11 +99,11 @@ export const OrderPerformanceCard = () => {
     </RadioGroup>)
 }
 
-const performanceCards = [
-  { category: 'orders', amount: 100, percentage: 50, previousYear: 50 },
-  { category: 'net sales', amount: 50, percentage: 50, previousYear: 25 },
-  { category: 'items sold', amount: 50, percentage: 50, previousYear: 25 },
-  { category: 'successful orders', amount: 50, percentage: 50, previousYear: 25 },
-  { category: 'kol orders', amount: 50, percentage: 50, previousYear: 25 },
-  { category: 'non-kol orders', amount: 50, percentage: 50, previousYear: 25 }
-]
+// const performanceCards = [
+//   { category: 'orderssss', amount: 100, percentage: 50, previousYear: 50 },
+//   { category: 'net sales', amount: 50, percentage: 50, previousYear: 25 },
+//   { category: 'items sold', amount: 50, percentage: 50, previousYear: 25 },
+//   { category: 'successful orders', amount: 50, percentage: 50, previousYear: 25 },
+//   { category: 'kol orders', amount: 50, percentage: 50, previousYear: 25 },
+//   { category: 'non-kol orders', amount: 50, percentage: 50, previousYear: 25 }
+// ]
