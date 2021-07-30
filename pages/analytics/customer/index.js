3910a3788fc99b4fa4c392c3323b7026ffa811f1
azwar <azwar.akbar@gmail.com>
2021-07-30 20:20:40 +0700

    Implement state management on customer page, add some listener for child component, make data updated based on filter changes

--- pages/analytics/customer/index.js
+++ pages/analytics/customer/index.js
@@ -14,26 +14,98 @@ import useFetch from 'hooks/useFetch'
 import { GET_ANALYTIC_CUSTOMER_TABLE } from 'graphql/customer'
 import { LoadingStatus } from 'components/loading-status'
 import { ErrorStatus } from 'components/error-status'
+import { DateRange } from 'components/widgets/DateRange'
+import { getAnalyticCustomerTable } from 'services/api/analytics_customer'
+import { timeFilterFormatter } from 'utils/Others'
 
 export default function Customer() {
+  const [dataTable, setDataTable] = useState([])
   const [selectedCurrent, setSelectedCurrent] = useState(dates[0])
   const [selectedPrevious, setSelectedPrevious] = useState(dates[1])
   const [selectedCustomer, setSelectedCustomer] = useState()
+  const [filter, setFilter] = useState()
+  const [startDate, setStartDate] = useState()
+  const [endDate, setEndDate] = useState()
+  
+  let isDataInitialized = false
+
+  // const { loading, error, data } = useFetch(GET_ANALYTIC_CUSTOMER_TABLE)
+
+  const initData = () => {
+    
+  }
 
   useEffect(() => {
     checkLogin()
   }, [])
 
+  // useEffect(() => {
+  //   if (data && data.getAnalyticCustomerTable && !isDataInitialized) {
+  //     setDataTable(data.getAnalyticCustomerTable)
+  //     isDataInitialized = true
+  //   }
+  // }, [data])
+
   useEffect(() => {
     console.log('selectedCustomer', selectedCustomer)
   }, [selectedCustomer])
 
-  const { loading, error, data } = useFetch(GET_ANALYTIC_CUSTOMER_TABLE)
+  useEffect(async () => {
+    console.log('filter:' + JSON.stringify(filter))
+    const result = await getAnalyticCustomerTable(filter)
+    console.log('result:', result)
 
-  if (loading) return <LoadingStatus />
-  if (error) return <ErrorStatus message={error} />
+    if (result.isSuccess) {
+      setDataTable(result.data)
+    }
 
-  const { getAnalyticCustomerTable } = data
+    const timeFilter = timeFilterFormatter(filter)
+
+    if (timeFilter) {
+      setStartDate(timeFilter.startDate)
+      setEndDate(timeFilter.endDate)
+    }
+  }, [filter])
+
+  useEffect(() => {
+  }, [dataTable])
+
+  // if (loading) return <LoadingStatus />
+  // if (error) return <ErrorStatus message={error} />
+
+  const dateRageChange = (dateRange) => {
+    console.log('dateRange', dateRange)
+    setFilter(prevState => ({
+      ...prevState,
+      ...dateRange
+    }))
+    // const _dateRange = {
+    //   mode: '24h',
+    //   dayStart: null,
+    //   dayEnd: null,
+    //   weekStart: null,
+    //   weekEnd: null,
+    //   monthStart: null,
+    //   monthEnd: null,
+    //   yearStart: null,
+    //   yearEnd: null
+    // }
+    // setDateRange(dateRange)
+  }
+
+  const keywordChange = (keyword) => {
+    setFilter(prevState => ({
+      ...prevState,
+      keyword: keyword
+    }))
+  }
+
+  const sortingChange = (sorting) => {
+    setFilter(prevState => ({
+      ...prevState,
+      sorting: sorting
+    }))
+  }
 
   return (
     <div className="pb-4">
@@ -42,20 +114,23 @@ export default function Customer() {
         {/* header */}
         <h2 className="w800">Analytics</h2>
         {/* second row */}
-        <DateRangeSelect
+        {/* <DateRangeSelect
           selectedCurrent={selectedCurrent}
           setSelectedCurrent={setSelectedCurrent}
           selectedPrevious={selectedPrevious}
           setSelectedPrevious={setSelectedPrevious}
-        />
+        /> */}
+        <DateRange onChange={dateRageChange}/>
         {/* customer_list-border */}
-        <div className="flex items-center justify-between mt-10 mb-4">
+        <div className="flex items-center justify-between mt-4 mb-4">
           <h4 className="capitalize w600 whitespace-nowrap">customer list</h4>
           <div className="w-full mx-5 border border-N0 border-opacity-30"></div>
           <BsThreeDotsVertical className="w-6 h-6 mr-2 text-N0" />
         </div>
         {/* table */}
-        <TableOrders data={getAnalyticCustomerTable} setSelectedCustomer={setSelectedCustomer} />
+        <TableOrders data={dataTable} setSelectedCustomer={setSelectedCustomer} 
+          keywordChange={keywordChange}
+          sortingChange={sortingChange}/>
         {/* pagination */}
         <Pagination />
         {/* customer_performance-border */}
@@ -67,7 +142,7 @@ export default function Customer() {
           <BsThreeDotsVertical className="w-6 h-6 mr-2 text-N0" />
         </div>
         {/* performance-cards */}
-        <OrderPerformanceCard selectedCustomer={selectedCustomer}/>
+        <OrderPerformanceCard selectedCustomer={selectedCustomer} startDate={startDate} endDate={endDate}/>
         {/* chart */}
         <ChartView />
       </div>
